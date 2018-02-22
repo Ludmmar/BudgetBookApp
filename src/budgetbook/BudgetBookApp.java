bc4a5505c20a2cb1f21c24e78718849f2d7af0fc
MARVIN-PC\MarvinLudmann <lummar@freenet.de>
2018-02-22 22:13:48 +0100

    Removed Warnings

--- src/budgetbook/BudgetBookApp.java
+++ src/budgetbook/BudgetBookApp.java
@@ -1,33 +1,19 @@
 package budgetbook;
 
 import budgetbook.controller.MainViewController;
-import budgetbook.controller.SaveController;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-import javafx.stage.Window;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import java.io.File;
-import java.util.List;
-import java.util.prefs.Preferences;
 
 public class BudgetBookApp extends Application {
 
-    private Stage primaryStage;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
-
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main_view.fxml"));
-        Parent root = (Parent)loader.load();
-        MainViewController controller = (MainViewController)loader.getController();
+        Parent root = loader.load();
+        MainViewController controller = loader.getController();
         controller.setBudgetBookApp(this);
 
         primaryStage.setTitle("Haushaltsbuch");
@@ -37,10 +23,6 @@ public class BudgetBookApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
     }
 }
 
